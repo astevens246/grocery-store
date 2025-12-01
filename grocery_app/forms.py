3deddd58007730b379728fb4043da51c28906d9e
Allen Stevens <allen.stevens@students.dominican.edu>
2025-12-01 11:32:04 -0800

    Add GroceryStoreForm and GroceryItemForm classes

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -7,19 +7,18 @@ class GroceryStoreForm(FlaskForm):
     """Form for adding/updating a GroceryStore."""
 
     # TODO: Add the following fields to the form class:
-    # - title - StringField
-    # - address - StringField
-    # - submit button
-    pass
+    title = StringField('Title', validators=[DataRequired(), Length(min=3, max=80)])
+    address = StringField('Address', validators=[DataRequired(), Length(min=3, max=200)])
+    submit = SubmitField('Submit')
+
 
 class GroceryItemForm(FlaskForm):
     """Form for adding/updating a GroceryItem."""
 
     # TODO: Add the following fields to the form class:
-    # - name - StringField
-    # - price - FloatField
-    # - category - SelectField (specify the 'choices' param)
-    # - photo_url - StringField
-    # - store - QuerySelectField (specify the `query_factory` param)
-    # - submit button
-    pass
+    name = StringField('Name', validators=[DataRequired(), Length(min=3, max=80)])
+    price = FloatField('Price', validators=[DataRequired()])
+    category = SelectField('Category', choices=ItemCategory.choices())
+    photo_url = StringField('Photo URL', validators=[DataRequired(), URL()])
+    store = QuerySelectField(query_factory=lambda: GroceryStore.query, get_label='title')
+    submit = SubmitField('Submit')
